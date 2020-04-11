215849796b7919b9545052783441782587ab5d90
marina <marina.vrataric@gmail.com>
2020-04-11 20:43:14 +0200

    fetched images, displayed gallery to homepage + css

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import FetchUrl from './FetchUrl'
 
 function HomePage() {
+
+    const fetchedData = FetchUrl('https://jsonplaceholder.typicode.com/photos')
+
+    const allPhotos = fetchedData.data.filter((i: { id: number }) => i.id < 9)
+    const photos = allPhotos.map((item: { id: any, url: string, title: string }) => {
+        return (
+            <div className="cards">
+                <div className="card">
+                    <img src={item.url} alt="img_colors" />
+                    <p className="item-title">{item.title}</p>
+                </div>
+            </div>
+        )
+    })
+
     return (
-        <div>
-            <h1 className="title">Home Page</h1>
+        <div className="homepage">
+            <h1 className="title">Home Page ~ Gallery</h1>
+            <div>{photos}</div>
         </div>
     )
 }
