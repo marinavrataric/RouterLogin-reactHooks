0b761ccd46bbcb57fb9c3c7ac3924f25fb3dc819
marina <marina.vrataric@gmail.com>
2020-04-11 16:05:05 +0200

    created empty components

---
+++ src/components/UserContact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserContact() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserContact
---
+++ src/components/UserPosts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserPosts() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserPosts
