fd6026bd0a676e91c9aa9dd21911147ee693baf4
marina <marina.vrataric@gmail.com>
2020-04-12 19:51:07 +0200

    added style and functionalities to UserContacts

--- src/components/UserContact.tsx
+++ src/components/UserContact.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import FetchUrl from './FetchUrl'
 
 function UserContact() {
+
+    const fetchedContacts = FetchUrl('https://jsonplaceholder.typicode.com/users')
+    const contacts = fetchedContacts.data.map((contact: {
+        name: string,
+        email: string,
+        phone: string,
+        address: { street: string, suite: string, city: string }
+    }) => {
+        return (
+            <tr>
+                <td>{contact.name}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phone}</td>
+                <td>{contact.address.street}, {contact.address.suite}, {contact.address.city}</td>
+            </tr>)
+    })
+
+
     return (
-        <div>
+        <div className="usercontacts">
             <h1 className="title">User Contacts</h1>
+            {fetchedContacts.isLoading && <h3>Loading...</h3>}
+            <table>
+                <thead>
+                    <tr className="row">
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Phone</td>
+                        <td>Address</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts}
+                </tbody>
+            </table>
         </div>
     )
 }
