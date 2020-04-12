9f10085408cca0d35402950875ca4fe8c0b62502
marina <marina.vrataric@gmail.com>
2020-04-12 14:16:26 +0200

    fixup! created App (main) component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import NavigationBar from './components/router/NavigationBar';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-      <NavigationBar />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
