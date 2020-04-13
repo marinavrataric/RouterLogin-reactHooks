74854f7a4d7b8a6c20a889a222fab0ad6f8908a9
marina <marina.vrataric@gmail.com>
2020-04-13 18:10:30 +0200

    added conditional rendering to redirect page depending on if the user is logged in or out

--- src/components/router/SwitchRouter.tsx
+++ src/components/router/SwitchRouter.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import HomePage from '../HomePage'
 import UserContact from '../UserContact'
 import UserPosts from '../UserPosts'
 import AboutMe from '../AboutMe'
+import { LoginUserContext } from '../LoginUserContext'
 import Login from '../Login'
 
 function SwitchRouter() {
+
+    const { user } = useContext(LoginUserContext)
+
     return (
         <Switch>
             <Route path="/" exact component={HomePage} />
-            <Route path="/contacts" component={UserContact} />
-            <Route path="/posts" component={UserPosts} />
-            <Route path="/aboutme" component={AboutMe} />
+            <Route path="/contacts" render={() => (user.isLogin ? <UserContact /> : <Redirect to="/" />)} />
+            <Route path="/posts" render={() => (user.isLogin ? <UserPosts /> : <Redirect to="/" />)} />
+            <Route path="/aboutme" render={() => (user.isLogin ? <AboutMe /> : <Redirect to="/" />)} />
             <Route path="/login" component={Login} />
         </Switch>
     )
